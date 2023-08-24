cb842205fcf078ea9231afd172459ede41b4e741
Anton Chen <contact@antonchen.ca>
2023-08-23 21:02:15 -0700

    Create new website with latex theme

--- js/settings.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-	let isDarkMode = localStorage.getItem("isDarkMode");
-	localStorage.setItem("isDarkMode", (isDarkMode === "true") ? "false" : "true");
-	setColors();
-}
-
-// document.getElementById("toggle-dark-mode").addEventListener("click", () => {
-// 	toggleDarkMode();
-// });
